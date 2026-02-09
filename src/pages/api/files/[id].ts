9463f2e6c60be380aeaa339f22beb0899334bedf
Faddah Wolf <my_biz@me.com>
2026-02-09 15:00:35 -0800

    Created /src/pages/api/files/[id].ts and create PUT, PATCH, DELETE, and GET single file from the file ID in params.

---
+++ src/pages/api/files/[id].ts
@@ -0,0 +1,75 @@
+import type { APIRoute } from 'astro';
+import { DatabaseStorage } from '../../../lib/db/storage';
+
+export const GET: APIRoute = async ({ params }) => {
+    const storage = new DatabaseStorage();
+    const file = await storage.getFile(Number(params.id));
+    
+    if (!file) {
+        return new Response(JSON.stringify({ message: "File not found" }), {
+            status: 404,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    }
+    
+    return new Response(JSON.stringify(file), {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' }
+    });
+};
+
+export const PUT: APIRoute = async ({ params: { id }, request  }) => {
+    const storage = new DatabaseStorage();
+    const file = await storage.getFile(Number(id));
+    
+    if (!file) {
+        return new Response(JSON.stringify({ message: "File not found" }), {
+            status: 404,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    }
+    
+    const body = await request.json();
+    const updatedFile = await storage.updateFile(Number(id), body);
+    
+    return new Response(JSON.stringify(updatedFile), {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' }
+    });
+};
+
+export const PATCH: APIRoute = async ({ params: { id }, request  }) => {
+    const storage = new DatabaseStorage();
+    const file = await storage.getFile(Number(id));
+    
+    if (!file) {
+        return new Response(JSON.stringify({ message: "File not found" }), {
+            status: 404,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    }
+    
+    const body = await request.json();
+    const updatedFile = await storage.updateFile(Number(id), body);
+    
+    return new Response(JSON.stringify(updatedFile), {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' }
+    });
+};
+
+export const DELETE: APIRoute = async ({ params: { id } }) => {
+    const storage = new DatabaseStorage();
+    const file = await storage.getFile(Number(id));
+    
+    if (!file) {
+        return new Response(JSON.stringify({ message: "File not found" }), {
+            status: 404,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    }
+    
+    await storage.deleteFile(Number(id));
+    
+    return new Response(null, { status: 204 });
+}
