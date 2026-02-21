881b49208dacadd8aa1a807503ea83ec1307ea56
Faddah Wolf <my_biz@me.com>
2026-02-20 16:31:24 -0800

    New Manifest to import for entry.mjs.

--- dist/server/entry.mjs
+++ dist/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_Cdu30Vxa.mjs';
-import { manifest } from './manifest_CVsa4dm8.mjs';
+import { manifest } from './manifest_DAqvnzPm.mjs';
 
 const serverIslandMap = new Map();;
 
