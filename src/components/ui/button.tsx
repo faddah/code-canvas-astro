f1599ba714b14b1c37ee2700accad5a3443873b5
Faddah Wolf <my_biz@me.com>
2026-02-20 19:42:07 -0800

    Added `;` at end of line 59.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -56,7 +56,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       />
     )
   },
-)
-Button.displayName = "Button"
+);
+Button.displayName = "Button";
 
 export { Button, buttonVariants }
