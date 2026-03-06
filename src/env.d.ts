914327b46af3cb4e30b0f0d141105a90d725483b
Faddah Wolf <my_biz@me.com>
2026-03-06 14:50:34 -0800

    Removed @clerk/astro/env reference, as that is all handled in App.tsx now, and added declaration for namespace of App.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
-/// <reference types="@clerk/astro/env" />
+
+declare namespace App {
+  interface Locals {
+    auth: () => {
+      userId: string | null;
+      sessionId: string | null;
+      orgId: string | null;
+      orgRole: string | null;
+      orgSlug: string | null;
+    };
+  }
+}
