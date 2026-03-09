3b2b47fdfbedf5240c188a37c745ee30d6a814c4
Faddah Wolf <my_biz@me.com>
2026-03-09 09:53:36 -0700

    Removed self.apigw_client which was AWS API Gateway v1, in favor of self.apigwv2_client which is for newer AWS API Gateway v2.

--- update_aws_deployment.py
+++ update_aws_deployment.py
@@ -228,7 +228,6 @@ class UpdateDeployer:
         # Boto3 clients
         self.ecr_client    = boto3.client("ecr",           region_name=AWS_REGION)
         self.lambda_client = boto3.client("lambda",        region_name=AWS_REGION)
-        self.apigw_client  = boto3.client("apigateway",   region_name=AWS_REGION)
         self.apigwv2_client = boto3.client("apigatewayv2", region_name=AWS_REGION)
         self.cf_client     = boto3.client("cloudfront")
         self.r53_client    = boto3.client("route53")
