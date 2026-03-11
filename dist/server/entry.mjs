f907a0d1996bb72a519314b8a8d82583e8af0314
Faddah Wolf <my_biz@me.com>
2026-03-11 15:37:20 -0700

    New import for manifest.

--- dist/server/entry.mjs
+++ dist/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from "./renderers.mjs";
 import { c as createExports, s as serverEntrypointModule } from "./chunks/_@astrojs-ssr-adapter_BPuWAfxv.mjs";
-import { manifest } from "./manifest_C2SVLvoz.mjs";
+import { manifest } from "./manifest_DqAwUX2H.mjs";
 const serverIslandMap = /* @__PURE__ */ new Map();
 ;
 const _page0 = () => import("./pages/_image.astro.mjs");
