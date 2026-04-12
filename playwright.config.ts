7419c264dce905eadfe11f76f66117a0192ac79f
Faddah Wolf <my_biz@me.com>
2026-04-11 20:42:45 -0700

    Reduced workers in process from 3 => 2, to prevent condition in Playwright where there's a race condition as the dev server couldn't serve fast enough under load w/ 3 workers & 7  projects.

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 1,
-  workers: process.env.CI ? 2 : 3,
+  workers: process.env.CI ? 2 : 2,
   reporter: "html",
   use: {
     baseURL: "http://localhost:4321",
